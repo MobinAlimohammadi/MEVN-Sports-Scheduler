bf1ecaa1abe4aec07741e19e1948da5c2525bd70
Mobin Alimohammadi <malimohammadi@bayviewglen.ca>
2025-04-11 14:42:46 -0400

    rename

---
+++ backend/models/league1.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const LeagueSchema = new mongoose.Schema({
+  sport: String,
+  season: String,
+  ageGroup: String,
+  division: String,
+  gender: String,
+});
+
+export default mongoose.model('League', LeagueSchema);
+//
